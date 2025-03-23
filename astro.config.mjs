585d7665071684dcff83b51b4aa88ec82273fdd2
Oliver Hughes <hughes.oliver88@gmail.com>
2025-03-24 00:10:49 +0100

    update deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site:"https://man-on-box.github.io",
+  base:"/boring-project-astro",
   experimental: {
     svg: true,
   },
